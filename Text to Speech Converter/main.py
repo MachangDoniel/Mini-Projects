ce8461f88cab7402ce610b53d5a7ea130e263901
MachangDoniel <donieltripura121@gmail.com>
2024-05-04 07:15:11 +0600

    Text to Speech Converter'

---
+++ Text to Speech Converter/main.py
@@ -0,0 +1,12 @@
+from gtts import gTTS
+import os
+
+f = open('text.txt')
+x = f.read()
+
+language = 'en-us'  # Use 'en-us' for a male voice
+
+audio = gTTS(text=x, lang=language, slow=False)
+
+audio.save("text.wav")
+os.system("text.wav")
